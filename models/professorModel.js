c5a2d377157e46c1ff70ab3ba2002a2ac30050bd
AndreLuisDaSilveira <andreluis_86@yahoo.com.br>
2019-11-26 23:39:42 -0200

    Adicionando validação de campos.

--- models/professorModel.js
+++ models/professorModel.js
@@ -2,9 +2,24 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var professorSchema = new Schema({
-    registration: Number,
-    name: String,
-    email: String,
+    registration:{
+        type: Number,
+        required:[true, "Cannot be blank"],
+        match: [/[0-9]/g,'Field invalid']
+    },
+    name: {
+        type: String, 
+        lowercase: true, 
+        required: [true, "Cannot be blank"], 
+        match: [/^[a-zA-Z0-9]+$/, 'Field invalid']
+
+    },
+    email: {
+        type: String,
+         lowercase: true, 
+         required: [true, "Cannot be blank"], 
+         match: [/\S+@\S+\.\S+/, 'Field invalid']
+    },
     telephone: String,
     birth_date: Date,
     address: String
